c0249992c1bab84bab0b6095f11429a53167a2f8
bpolishevsk <bpolishevsk@gmail.com>
2023-03-16 22:04:20 +0200

    remove mongodb uri

--- server.js
+++ server.js
@@ -17,15 +17,6 @@ mongoose
   })
   .then(() => console.log("DB Connected"));
 
-// mongoose.createConnection(
-//   "mongodb+srv://kateryna:monster@1992@cluster0.ivsvnno.mongodb.net/metrics_book?retryWrites=true&w=majority",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-// app.use(bodyParser.json({ limit: '30mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 //middlewares
 app.use(bodyParser.json());
@@ -36,15 +27,7 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 app.use("/api/auth", authRountes);
 app.use("/api/metrics", metricsRountes);
 
-// Serve static assets in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
 
-//   app.get("*", (_req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
 
 const port = process.env.PORT || 8000;
 
