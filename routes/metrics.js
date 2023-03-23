8737c514235e3f64a1abb482d7d920b52ec84756
julianyeo <julianyeo26@gmail.com>
2023-03-23 13:27:19 +0800

    feat wages

--- routes/metrics.js
+++ routes/metrics.js
@@ -6,11 +6,25 @@ const {
   deleteMetricById,
   updateMetrics,
 } = require("../controllers/metrics.js");
+const {
+  addMetricsWage,
+  updateMetricsWage,
+  getUserMetricsAllWages,
+  getUserMetricsAllTodayWages,
+  deleteMetricWageById,
+  getLastestMetricsWagesById,
+} = require("../controllers/wage.js");
 const auth = require("../middleware/auth");
 
 router.post("", auth, addMetrics);
 router.put("", auth, updateMetrics);
 router.get("", auth, getUserMetrics);
 router.delete("", auth, deleteMetricById);
+router.post("/wage", auth, addMetricsWage);
+router.put("/wage", auth, updateMetricsWage);
+router.get("/wage", auth, getUserMetricsAllWages);
+router.get("/wage/today", auth, getUserMetricsAllTodayWages);
+router.delete("/wage", auth, deleteMetricWageById);
+router.get("/wage/lastest", auth, getLastestMetricsWagesById);
 
 module.exports = router;
