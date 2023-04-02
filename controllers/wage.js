a91f6d109f0e90248a68199c66dc7ac728d176ba
julianyeo <julianyeo26@gmail.com>
2023-04-03 02:09:49 +0800

    feat blood pressure api

--- controllers/wage.js
+++ controllers/wage.js
@@ -2,6 +2,7 @@ const Wage = require("../models/wage");
 const { check, validationResult } = require("express-validator");
 
 (exports.addMetricsWage = async (req, res) => {
+  console.log(req.body, "----------");
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
